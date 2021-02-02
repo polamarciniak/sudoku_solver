b02e0f81c5eb3ab27b32361e0c29f1e623524551
polamarciniak <74494320+polamarciniak@users.noreply.github.com>
2021-02-02 01:05:57 +0100

    main file

--- sudoku.cpp
+++ sudoku.cpp
@@ -1,9 +1,48 @@
+#include "head.h"
+#include "n_sudoku.h"
 #include "plansza.h"
 
-using namespace std;
+/*using namespace std;
 
 int main()
 {
     if(solve() == true)    print_sudoku();
     else    cout << "no solution";
+}*/
+
+using namespace std;
+
+int sizex, sizey;
+
+int main()
+{
+    initscr();
+    start_color();
+
+    getmaxyx(stdscr, sizey, sizex);
+
+    init_color_pairs();
+    refresh();
+    noecho();
+    curs_set(0);
+
+    WINDOW *sudoku;
+    attron(COLOR_PAIR(3));
+    write_entry();
+    attroff(COLOR_PAIR(3));
+    refresh();
+    getchar();
+    clear();
+    refresh();
+
+
+
+    if(solve() == true) //&& is_correct() == true) //nie dziala??
+    {
+        sudoku = create_window(11, 21, (sizey)/2-5, (sizex)/2-11, TRUE);
+        print_window(sudoku);
+    }
+    else    write_no_success();
+
+    return 0;
 }
